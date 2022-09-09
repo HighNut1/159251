36566cf5469b58a6946f38599fac187d8af067ce
Gaoyi <3311634983@qq.com>
2022-09-09 13:56:10 +0800

    Added featuer 1

--- Greeter.java
+++ Greeter.java
@@ -4,7 +4,7 @@ import java.util.ResourceBundle;
 public class Greeter {
 
   private Locale locale;
-  private String name;
+  private String name.UpperCase();
   private int age;
 
   public Greeter(String language, String country, String name) {
